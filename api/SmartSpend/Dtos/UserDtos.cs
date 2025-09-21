e609048aa901942f4d7e3a74457720c274483355
Karan Vyas <karanvyas2907@gmail.com>
2025-09-21 21:01:01 +0530

    apply user migration

---
+++ api/SmartSpend/Dtos/UserDtos.cs
@@ -0,0 +1,21 @@
+﻿namespace SmartSpend.Dtos
+{
+    public abstract class UserBaseDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
+    }
+
+    public class UserRegisterDto : UserBaseDto
+    {
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+
+    public class UserResponseDto : UserBaseDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public DateTime? CreatedAt { get; set; }
+    }
+}
---
+++ api/SmartSpend/Validators/UserRegisterValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using SmartSpend.Dtos;
+
+namespace SmartSpend.Validators
+{
+    public class UserRegisterValidator: AbstractValidator<UserRegisterDto>
+    {
+        public UserRegisterValidator() {
+            RuleFor(x => x.UserName)
+               .NotEmpty().WithMessage("Username is required")
+               .MinimumLength(3).WithMessage("Username must be at least 3 characters long");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid email format");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters long");
+
+            RuleFor(x => x.ConfirmPassword)
+                .Equal(x => x.Password).WithMessage("Passwords do not match");
+
+            RuleFor(x => x.PhoneNumber)
+                .Matches(@"^\+?[1-9]\d{1,14}$")
+                .When(x => !string.IsNullOrEmpty(x.PhoneNumber))
+                .WithMessage("Invalid phone number format");
+        }
+    }
+}
